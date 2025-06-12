aaecc31f184d971affa30fc954526818846b911f
Aashis719 <aashishnep10@gmail.com>
2025-06-12 06:58:17 +0545

    API key managed

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
-
+// import styles from "./page.module.css";
 export default function Home() {
   return (
      <>
